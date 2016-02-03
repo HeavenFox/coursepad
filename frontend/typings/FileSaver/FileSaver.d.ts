f276bbed66ff838e84a6e55cfa2ab1ddc3ac2be0
Jingsi Zhu <zhu@jing.si>
2016-02-02 20:44:30 -0500

    Vendor typings

---
+++ frontend/typings/FileSaver/FileSaver.d.ts
@@ -0,0 +1,37 @@
+// Type definitions for FileSaver.js
+// Project: https://github.com/eligrey/FileSaver.js/
+// Definitions by: Cyril Schumacher <https://github.com/cyrilschumacher>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/**
+ * @summary Interface for "saveAs" function.
+ * @author  Cyril Schumacher
+ * @version 1.0
+ */
+interface FileSaver {
+    (
+        /**
+         * @summary Data.
+         * @type {Blob}
+         */
+        data: Blob,
+        
+        /**
+         * @summary File name.
+         * @type {DOMString}
+         */
+	 filename: string,
+
+	 /**
+	  * @summary Disable Unicode text encoding hints or not.
+	  * @type {boolean}
+	  */
+	 disableAutoBOM?: boolean
+    ): void
+}
+
+declare module 'filesaver' {
+    var saveAs: FileSaver;
+    export default saveAs;
+}
+
