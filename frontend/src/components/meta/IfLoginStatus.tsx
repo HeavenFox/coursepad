f89da2b928ffc601feddc9b2235e0653d4b91e38
Jingsi Zhu <zhu@jing.si>
2019-07-13 15:44:24 -0400

    Bug fixes

--- frontend/src/components/meta/IfLoginStatus.tsx
+++ frontend/src/components/meta/IfLoginStatus.tsx
@@ -1,34 +1,45 @@
-import React from 'react';
-import user from '../../store/user';
+import React from "react";
+import user from "../../store/user";
 
 interface IfLoginStatusState {
-    loggedIn: boolean;
+  loggedIn: boolean;
 }
 
-export default class IfLoginStatus extends React.Component<React.Props<IfLoginStatus>, IfLoginStatusState> {
-    state = {loggedIn: user.loggedIn()};
+export default class IfLoginStatus extends React.Component<
+  IfLoginStatus,
+  IfLoginStatusState
+> {
+  state = { loggedIn: user.loggedIn() };
 
-    _update() {
-        this.setState({loggedIn: user.loggedIn()});
-    }
+  _update = () => {
+    this.setState({ loggedIn: user.loggedIn() });
+  };
 
-    componentDidMount() {
-        user.on('loginstatuschange', this._update);
-    }
+  componentDidMount() {
+    user.on("loginstatuschange", this._update);
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextState.loggedIn !== this.state.loggedIn;
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.loggedIn !== this.state.loggedIn;
+  }
 
-    render() {
-        if (this.state.loggedIn) {
-            return (
-                <div>{Array.isArray(this.props.children) ? this.props.children[0] : this.props.children}</div>
-            );
-        } else {
-            return (
-                <div>{Array.isArray(this.props.children) ? (this.props.children[1] || null) : null}</div>
-            );
-        }
+  render() {
+    if (this.state.loggedIn) {
+      return (
+        <div>
+          {Array.isArray(this.props.children)
+            ? this.props.children[0]
+            : this.props.children}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {Array.isArray(this.props.children)
+            ? this.props.children[1] || null
+            : null}
+        </div>
+      );
     }
+  }
 }
